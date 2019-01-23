848142c266b995a39ef8ad734bba39d8c99052b6
Melvin <SuperMelvin@users.noreply.github.com>
2019-01-23 11:34:06 +0800

    console

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,17 +7,20 @@ Page({
   },
   //事件处理函数
   getUserInfo: function (e) {
+    console.log('微信登录--------')
     console.log(e)
     if (e.detail.errMsg == 'getUserInfo:ok') {
       app.globalData.userInfo = e.detail.userInfo
       wx.reLaunch({
         url: '../index/index',
       })
+      console.log('获取用户信息成功--------')
       console.log(app.globalData.userInfo)
     }
   },
 
   onLoad: function (options) {
+    console.log('查看用户信息--------')
     console.log(app.globalData.userInfo)
   },
 
