4dc0577a0d5e4e712570515ba6bcc4531cbc12f8
Melvin <SuperMelvin@users.noreply.github.com>
2019-01-24 16:27:31 +0800

    完善index

--- pages/index/index.js
+++ pages/index/index.js
@@ -25,10 +25,69 @@ Page({
         name: 'Arctic Monkeys',
         avatar: 'https://i.scdn.co/image/bcc0dd361d61cdef1d34a27f850e14de6740521a',
       }
+    ],
+    album: [
+      {
+        name: 'Your Top Songs',
+        avatar: 'https://lineup-images.scdn.co/your-top-songs-2018_DEFAULT-en.jpg',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Origins',
+        avatar: 'https://i.scdn.co/image/6ac2ed0481154149e2ca501b5dcbe3c1ac937c43',
+        singer: 'Imagine Dragons'
+      },
+      {
+        name: 'Rock This',
+        avatar: 'https://pl.scdn.co/images/pl/default/e1b8b54f96cdc0255a6b824e73e6665e2236c04a',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Sick Boy',
+        avatar: 'https://i.scdn.co/image/77eb7c17cafe550354acb0696023249c769604d2',
+        singer: 'The Chainsmokers'
+      },
+      {
+        name: 'This is James Bay',
+        avatar: 'https://pl.scdn.co/images/pl/default/47446ebd2583be53d9d171147792628597da7139',
+        singer: 'Yfitops'
+      }
+    ],
+    playlist: [
+      {
+        name: '90s Hip-Hop',
+        avatar: 'https://pl.scdn.co/images/pl/default/e488f79f331bdd6388449565739ee9d194a99c8d',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Pop Rising',
+        avatar: 'https://pl.scdn.co/images/pl/default/bd4f8e2ae61561ca4854975a9a1db2ab5a68f1d7',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'mint',
+        avatar: 'https://pl.scdn.co/images/pl/default/39f25c41599c05278479e4f02ed147217eb53e0d',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Country Hits',
+        avatar: 'https://pl.scdn.co/images/pl/default/799842421713e8a40f41ec64979c1aa2efd43f31',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Chilled R&B',
+        avatar: 'https://pl.scdn.co/images/pl/default/a4af47fc2ff189edd0beaaa64fa70be62c6998eb',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Peaceful Piano',
+        avatar: 'https://pl.scdn.co/images/pl/default/56228f9353b23405516a6ea8af1c22083f450b57',
+        singer: 'Yfitops'
+      }
     ]
   },
   //事件处理函数
-
+  
   onLoad: function (options) {
     
   },
