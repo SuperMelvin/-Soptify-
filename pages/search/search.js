ed97b4f209235c46777a6b58df718c6ca5259bea
Melvin <SuperMelvin@users.noreply.github.com>
2019-01-25 16:47:24 +0800

    user

--- pages/search/search.js
+++ pages/search/search.js
@@ -4,7 +4,38 @@ const app = getApp()
 
 Page({
   data: {
-
+    playlist: [
+      {
+        name: '90s Hip-Hop',
+        avatar: 'https://pl.scdn.co/images/pl/default/e488f79f331bdd6388449565739ee9d194a99c8d',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Pop Rising',
+        avatar: 'https://pl.scdn.co/images/pl/default/bd4f8e2ae61561ca4854975a9a1db2ab5a68f1d7',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'mint',
+        avatar: 'https://pl.scdn.co/images/pl/default/39f25c41599c05278479e4f02ed147217eb53e0d',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Country Hits',
+        avatar: 'https://pl.scdn.co/images/pl/default/799842421713e8a40f41ec64979c1aa2efd43f31',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Chilled R&B',
+        avatar: 'https://pl.scdn.co/images/pl/default/a4af47fc2ff189edd0beaaa64fa70be62c6998eb',
+        singer: 'Yfitops'
+      },
+      {
+        name: 'Peaceful Piano',
+        avatar: 'https://pl.scdn.co/images/pl/default/56228f9353b23405516a6ea8af1c22083f450b57',
+        singer: 'Yfitops'
+      }
+    ]
   },
   //事件处理函数
 
---
+++ pages/user/user.js
@@ -0,0 +1,30 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    favorite: 'https://i.scdn.co/image/e797bac3e4483e208fed94c95151ee327694db4d',
+  },
+  //事件处理函数
+
+  onLoad: function (options) {
+    
+  },
+
+  onShow () {
+
+  },
+
+  onReady() {
+
+  },
+
+  onHide() {
+
+  },
+
+  onUnload() {
+
+  },
+})
